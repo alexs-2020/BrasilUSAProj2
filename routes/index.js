ca07e34e9835677eb0a03383db87e4369c6d2545
Alex Smith <axsmith2020@gmail.com>
2022-07-05 10:49:28 +0200

    working authentication

--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,33 @@ router.get("/add", (req, res, next) => {
   res.render('add')
 })
 
+//auth
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+const User = require('../models/User.model')
+
+
+//auth routes
+router.get('/signup', (req,res)=>{
+  res.render('auth/signup')
+})
+
+router.post('/signup', (req,res,next)=>{
+  const { username, password } = req.body;
+ 
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+      return User.create({
+        username: username,
+        password: hashedPassword
+      })
+    })
+    .then(userFromDB =>{
+      res.redirect('/')
+    })
+    .catch(error => next(error));
+});
 
 module.exports = router;
