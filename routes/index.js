0d0a82edf7c7ba19ef8ce3e0ab9133070a9fb7f7
Alex Smith <axsmith2020@gmail.com>
2022-07-05 15:02:13 +0200

    View organization

--- routes/index.js
+++ routes/index.js
@@ -7,31 +7,34 @@ router.post('/addLocation', (req, res) => {
   Location.create(req.body)
       .then(() => {
         console.log('wewe')
-        res.redirect('/')})
+        res.redirect('/map')})
       .catch(error => console.log(error));
 });
 
 router.get("/", (req, res, next) => {
-  Location.find()
-  .then((locationFromDB) => {
-    res.render("index", { locationList: locationFromDB });
-  })
-  .catch((err) => {
-      next(err)
-  })
+  res.render('index')
 })
 router.get("/add", (req, res, next) => {
   res.render('add')
 })
+
+router.get('/map', (req,res)=> 
+  Location.find()
+  .then((locationFromDB) => {
+    res.render("map", { locationList: locationFromDB });
+  })
+  .catch((err) => {
+      next(err)
+  })
+  )
 
 //auth
 const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 const User = require('../models/User.model')
 
-
 //auth routes
-router.get('/signup', (req,res)=>{
+router.get('/signup', (req, res)=>{
   res.render('auth/signup')
 })
 
@@ -52,5 +55,15 @@ router.post('/signup', (req,res,next)=>{
     })
     .catch(error => next(error));
 });
+
+router.post('/locations/:locationId/delete', (req, res, next) => {
+  const { locationId } = req.params;
+
+  Location.findByIdAndDelete(locationId)
+    .then(() => res.redirect('/map'))
+    .catch(error => next(error));
+});
+
+
 
 module.exports = router;
